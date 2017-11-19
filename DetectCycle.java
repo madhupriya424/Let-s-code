4dde2585413425dce54eada6c7ef40a8b66517e0
madhupriya424 <madhupriya424@gmail.com>
2017-11-19 23:59:52 +0530

    Update DetectCycle.java

--- DetectCycle.java
+++ DetectCycle.java
@@ -55,3 +55,70 @@ class LinkedList {
 
 O/p:
 false
+# Through HashMap:
+
+
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int data){
+        this.data=data;
+        next=null;
+    }
+}
+
+
+
+class LinkedList {
+    static Node head1;
+    static Node Even=null;
+    static Node Odd=null;
+    static Node EvenFirst=null;
+  
+    
+    
+   static boolean detectLoop(Node h)
+    {
+        HashSet<Node> s = new HashSet<Node>();
+        while (h != null)
+        {
+             // If we have already has this node
+             // in hashmap it means their is a cycle
+             // (Because you we encountering the
+             // node second time).
+            if (s.contains(h))
+                return true;
+  
+            // If we are seeing the node for
+            // the first time, insert it in hash
+            s.add(h);
+  
+            h = h.next;
+        }
+  
+        return false;
+    }
+    
+	public static void main (String[] args) {
+	    
+        LinkedList ll = new LinkedList();
+        ll.head1= new Node(1);
+        ll.head1.next=new Node(2);
+        ll.head1.next.next=new Node(3);
+        ll.head1.next.next.next=new Node(4);
+        
+        ll.head1.next.next.next = ll.head1;
+      
+	System.out.print(detectLoop(head1));
+	  
+	}
+	
+}
+
+O/P:
+true
+	
