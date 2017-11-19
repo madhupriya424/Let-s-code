92a4d4abce1987fd3ec6a1853d56181bfdc54d5f
madhupriya424 <madhupriya424@gmail.com>
2017-11-19 22:17:08 +0530

    Update ReaarangeEvenOddNoLL.java

--- ReaarangeEvenOddNoLL.java
+++ ReaarangeEvenOddNoLL.java
@@ -1,6 +1,6 @@
 Input:   1->2->3->4
-Output:  1->3->2->4
-
+Output:  1->3->2->4	
+	
 Input:   10->22->30->43->56->70
 Output:  10->30->56->70->22->43
 
@@ -49,9 +49,9 @@ class LinkedList {
     }
     
     if(temp==null){
-        Odd.next=EvenFirst;
+        EvenFirst.next=Odd;
     }
-    return head;
+    return Even;
     }
     
     
@@ -71,3 +71,6 @@ class LinkedList {
 	}
 	}
 }
+
+O/P:
+4-> 2-> 5-> 3-> 1
